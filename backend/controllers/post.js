750add3ae66fefc113dc96b624a2aef600981622
Izuchukwu <izunduche29@gmail.com>
2025-03-24 18:27:05 +0100

    Changes Have Been Made

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,19 +1,33 @@
 const post = require('../model/post')
 
 const getAllPost = async (req ,res) => {
+    try{
     const getPost = await post.find({})
     res.status(200).json({getPost})
+    }
+    catch(error){
+    console.log(error)
+    }
 }
+
 const createPost = async (req ,res) => {
+    try{
     const { title , author , content } = req.body
     // const postBody = {  }
     const postBody = await post.create({title , author, content})
     res.status(200).json(postBody)
     console.log("Message Was Sent !!!")
+    }
+    catch(error){
+    console.log(error)
+    }
 }
+
 const getSinglePost = async (req ,res) => {
     try{
-        const getPost = await post.find({})
+        const userId = req.params.id
+        console.log(typeof userId)
+        const getPost = await post.findOne({ _id : userId })
         res.status(200).json({getPost})
     }
     catch(error){
