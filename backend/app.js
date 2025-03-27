e6bb14d43b2c7e22c5926b34f7e7fe50572c3b48
Izuchukwu <izunduche29@gmail.com>
2025-03-27 13:57:06 +0100

    Changes Have Been Made

--- backend/app.js
+++ backend/app.js
@@ -8,11 +8,13 @@ const commentRoute = require('./routes/comment')
 const connectDB = require('./db/connect')
 
 app.use(express.json())
+
 app.use('/api' , postroute)
 app.use('/user' , userRoute)
 app.use('/protect' , infoRoute)
 app.use('/comments' , commentRoute)
-    app.listen(5000 , () => {
+
+app.listen(5000 , () => {
     connectDB(process.env.MONGO_URI)
     console.log('Server Is Listening on port 5000')
 })
