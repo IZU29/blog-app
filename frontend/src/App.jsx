9943233c363d998be01a8fcc77a148ed2fd37241
Izuchukwu <izunduche29@gmail.com>
2025-05-30 15:44:11 +0100

    Changes just made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -16,18 +17,37 @@ function App() {
   }
   return (
     
+    // <Router>
+    // <>
+    // <NavBar />
+    // {/* {
+    // (sign)?
+    // <Signpage 
+    // choosePage = {choosePage}
+    // />:
+    // <SignIn 
+    // choosePage = {choosePage}/>
+    // } */}
+    // <Blog />
+    // <SingleBlog />
+    // </>
+    // <Routes>
+    // <Route path="/" element={<Home />} />
+    // <Route path="/about" element={<About />} />
+    // </Routes>
+    // </Router>
     <>
     <NavBar />
-    {/* {
-    (sign)?
-    <Signpage 
-    choosePage = {choosePage}
-    />: 
-    <SignIn 
-    choosePage = {choosePage}/>
-    } */}
-    {/* <Blog /> */}
-    <SingleBlog />
+    <Router>
+    <Routes>
+    <Route path="/blog" element={<Blog />} />
+    <Route path="/single" element={<SingleBlog/>} />
+    <Route path="/register" element={<SignIn/>} />
+    <Route path="/login" element={<Signpage />} />
+    <Route path="/single" element={<SingleBlog/>} />
+    <Route path="/single" element={<SingleBlog/>} />
+    </Routes>
+    </Router>
     </>
   )
 }
--- frontend/src/Blog.jsx
+++ frontend/src/Blog.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Blog = () => {
 let text = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore quos impedit, optio quas, aliquid voluptatibus sit a quaerat amet soluta fuga iusto est distinctio maxime dolorum maiores? Molestias quos, modi quae repudiandae neque facilis vitae aut voluptas fugiat iusto id ratione eaque assumenda consectetur architecto suscipit minus eum. Dolorem, mollitia!"
 let maxLength = 150
@@ -9,11 +9,13 @@ let maxLength = 150
         <input type="text" name="" id="" className="rounded-[200px] my-4 bg-[#e3e3e3] p-2 w-[90%] self-center" placeholder='Search Blog Post'/>
         </div>
         <div className="grid grid-cols-1 gap-4">
+        {/* <Link to ='/single'> */}
             <div className="border border-grey my-2 w-[85%] m-auto rounded-lg p-2">
                 <h2 className="text-2xl font-semibold truncate ">Lorem, ipsum dolor.</h2>
                 <h3 className="text-[#0500FF]">Lorem.</h3>
                 <p className="">{text.length > maxLength? text.substring(0 ,maxLength) + '...': text}</p>
             </div>
+            {/* </Link> */}
             <div className="border border-gray my-2 w-[85%] m-auto rounded-lg p-2"><h2 className="text-2xl font-semibold truncate ">Lorem, ipsum dolor.</h2>
                 <h3 className="text-[#0500FF]">Lorem.</h3>
                 <p className="">{text.length > maxLength? text.substring(0 ,maxLength) + '...': text}</p></div>
